179fa4cf4fa4b6c796208e918ed6226f91dbe5ee
Mafinar Khan <mafinar@gmail.com>
2020-04-27 02:34:59 -0400

    Refactor session controller

--- lib/karma_werks_web/controllers/session_controller.ex
+++ lib/karma_werks_web/controllers/session_controller.ex
@@ -1,15 +1,18 @@
 defmodule KarmaWerksWeb.SessionController do
-  alias Phoenix.LiveView
   alias KarmaWerksWeb.HomeLive
   alias KarmaWerksWeb.Auth.SigninLive
+  alias KarmaWerksWeb.Router.Helpers, as: Routes
+  alias KarmaWerksWeb.Endpoint
 
   use KarmaWerksWeb, :controller
 
   def create(conn, %{"user" => %{"token" => token}}) do
-    LiveView.Controller.live_render(put_session(conn, "token", token), HomeLive)
+    home = Routes.live_path(Endpoint, HomeLive)
+    redirect(put_session(conn, "token", token), to: home)
   end
 
   def destroy(conn, _params) do
-    LiveView.Controller.live_render(clear_session(conn), SigninLive)
+    signin = Routes.live_path(Endpoint, SigninLive)
+    redirect(clear_session(conn), to: signin)
   end
 end
